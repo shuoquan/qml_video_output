eed32f6eefc5d8a0a4631740a18d58fda5f50efb
dingshuoquan <dingshuoquan@oceandark.com>
2022-11-08 18:17:40 +0800

    add home cpp

---
+++ home.cpp
@@ -0,0 +1,60 @@
+﻿#include "home.h"
+#include <QDebug>
+
+Home::Home(QObject *parent) : QObject(parent)
+{
+    qDebug() << "home";
+}
+
+
+Home::~Home()
+{
+}
+
+void Home::fetchBag(int bagId, int type, int ps) {
+    qDebug() << "fetchBag";
+    qDebug() << bagId;
+    QString url = "http://192.168.8.117:3000/bag";
+    url += "?ps=" + QString::number(ps);
+    if (bagId == 0) {
+        // 初始化时寻找前5分钟的前2个包
+        QDateTime timeDate = QDateTime::currentDateTime();  // 获取当前时间
+        int endTime = timeDate.toTime_t();
+        int startTime = endTime - 300;
+        url += "&start=" + QString::number(startTime) + "000";
+        url += "&end=" + QString::number(endTime) + "000";
+    } else {
+        url += "&id=" + QString::number(bagId);
+        url += "&type=" + QString::number(type);
+    }
+    qDebug() << url;
+    QNetworkRequest request;
+    QNetworkAccessManager manager;
+    connect(&manager, &QNetworkAccessManager::finished, this, &Home::receiveReply);
+
+    //get与post的请求方式有所不同，get是在接口名后添加 ? 和传输的数据(type)
+    request.setUrl(QUrl(url));
+    request.setHeader(QNetworkRequest::ContentTypeHeader,QVariant("application/json;charset=utf-8"));
+    QNetworkReply *reply = manager.get(request);    //get请求头
+
+    //开启事件循环，直到请求完成
+    QEventLoop loop;
+    connect(reply,&QNetworkReply::finished,&loop,&QEventLoop::quit);
+    loop.exec();
+}
+
+
+void Home::receiveReply(QNetworkReply *reply)
+{
+    QString res = reply->readAll();
+//    qDebug() << "receiveRes" << res;
+    emit sendBagInfo(res);
+}
+
+
+
+int Home::test(int id)
+{
+    qDebug() << "test" << id;
+    return 234;
+}
---
+++ home.h
@@ -0,0 +1,35 @@
+﻿#ifndef HOME_H
+#define HOME_H
+
+#include <QObject>
+#include <QTime>
+#include <QTimer>
+#include <QDebug>
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QDateTime>
+#include <QHttpMultiPart>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QMetaObject>
+#include <QEventLoop>
+
+
+#pragma execution_character_set("utf-8")
+
+class Home : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Home(QObject *parent = nullptr);
+    ~Home();
+    Q_INVOKABLE void fetchBag(int bagId = 0, int type = 0, int ps = 1);
+    Q_INVOKABLE int test(int id);
+    void receiveReply(QNetworkReply *reply);
+
+signals:
+    void sendBagInfo(QString bagInfo);
+
+};
+
+#endif // HOME_H
